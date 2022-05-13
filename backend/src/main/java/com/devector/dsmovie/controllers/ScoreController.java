2ef6451951a872fbf2ba8528db4c72466c590f95
DeVector <diniz.vito@gmail.com>
2022-05-13 10:45:03 -0300

    First homolog

--- backend/src/main/java/com/devector/dsmovie/controllers/ScoreController.java
+++ backend/src/main/java/com/devector/dsmovie/controllers/ScoreController.java
@@ -2,7 +2,6 @@ package com.devector.dsmovie.controllers;
 
 import com.devector.dsmovie.dto.MovieDTO;
 import com.devector.dsmovie.dto.ScoreDTO;
-import com.devector.dsmovie.services.MovieService;
 import com.devector.dsmovie.services.ScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
